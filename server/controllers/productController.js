d3ec16d8d4724d9dcd282e72fc232b4cc8ebc06e
Feecon <mail2feeconbehera@gmail.com>
2024-02-06 12:09:36 +0530

    crud factory design implemented

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,142 +1,22 @@
 const Product = require("../models/productModel")
+const {
+  createFactory,
+  getFactory,
+  getFactoryById,
+  updateFactoryById,
+  deleteFactoryById,
+  checkInput,
+} = require("../utils/crudFactory");
 
-const checkInput = function (req, res, next) {
-  console.log("req method", req.method);
-  const productDetails = req.body;
-  const isEmpty = Object.keys(productDetails).length === 0;
-  if (isEmpty) {
-    res.status(400).json({
-      status: 400,
-      message: "Body cannot be empty",
-    });
-  } else {
-    next();
-  }
-};
-
-async function createProduct(req, res) {
-  try {
-    console.log("request body", req.body);
-    const productDetails = req.body;
-    const isEmpty = Object.keys(productDetails).length === 0;
-    if (isEmpty) {
-      res.status(400).json({
-        status: 400,
-        message: "Body cannot be empty",
-      });
-    } else {
-      console.log("new product", productDetails);
-      const product = await Product.create(productDetails);
-      res.status(201).json({
-        status: 201,
-        message: "Product created Successfully!",
-        data: product,
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      status: 500,
-      message: error.message,
-    });
-  }
-}
-
-async function getProduct(req, res) {
-  try {
-    const productData = await Product.find();
-    if (productData.length === 0) {
-      throw new Error("Product not found");
-    } else {
-      res.status(200).json({
-        status: 200,
-        message: "Data found",
-        data: productData,
-      });
-    }
-  } catch (err) {
-    res.status(500).json({
-      status: 500,
-      message: err.message,
-    });
-  }
-}
-
-async function getProductById(req, res) {
-  try {
-    const { id } = req.params;
-    console.log("productid", id);
-    const product = await Product.findById(id);
-    if (!product) {
-      throw new Error("Product not found");
-    }
-    res.status(200).json({
-      status: 200,
-      message: "Product found",
-      data: product,
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 500,
-      message: err.message,
-    });
-  }
-}
-
-async function updateProductById(req, res) {
-  try {
-    console.log("request params", req.params);
-    const { id } = req.params;
-    const updatedProductData = req.body;
-    console.log("productid", id);
-    const updatedProduct = await Product.findByIdAndUpdate(
-      id,
-      updatedProductData,
-      {
-        new: true,
-      }
-    );
-    console.log("updated product", updatedProduct);
-    if (!updatedProduct) {
-      throw new Error("Product not found");
-    } else {
-      res.status(200).json({
-        status: 200,
-        message: "Product updated successfully",
-        data: updatedProduct,
-      });
-    }
-  } catch (err) {
-    res.status(500).json({
-      status: 500,
-      message: err.message,
-    });
-  }
-}
-
-async function deleteProductById(req, res) {
-  try {
-    const { id } = req.params;
-    const deletedProduct = await Product.findByIdAndDelete(id);
-    if (!deletedProduct) {
-      throw new Error("Product not found");
-    } else {
-      res.status(200).json({
-        status: 200,
-        message: "Product deleted successfully",
-        data: deletedProduct,
-      });
-    }
-  } catch (err) {
-    res.status(500).json({
-      status: 500,
-      message: err.message,
-    });
-  }
-}
+const getProduct = getFactory(Product);
+const createProduct = createFactory(Product);
+const getProductById = getFactoryById(Product);
+const updateProductById = updateFactoryById(Product);
+const deleteProductById = deleteFactoryById(Product); 
 
 module.exports = {
-  createProduct,
   getProduct,
+  createProduct,
   getProductById,
   updateProductById,
   deleteProductById,
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,138 +1,18 @@
-const User = require('../models/userModel')
+const User = require('../models/userModel');
+const {
+  createFactory,
+  getFactory,
+  getFactoryById,
+  updateFactoryById,
+  deleteFactoryById,
+  checkInput
+} = require('../utils/crudFactory');
 
-const checkInput = function (req, res, next) {
-  console.log("req method", req.method);
-  const userDetails = req.body;
-  const isEmpty = Object.keys(userDetails).length === 0;
-  if (isEmpty) {
-    res.status(400).json({
-      status: 400,
-      message: "Body cannot be empty",
-    });
-  } else {
-    next();
-  }
-}
-
-async function getUser(req, res) {
-  try {
-    const userData = await User.find();
-    if (userData.length === 0) {
-      throw new Error("User not found");
-    } else {
-      res.status(200).json({
-        status: 200,
-        message: "Data found",
-        data: userData,
-      });
-    }
-  } catch (err) {
-    res.status(500).json({
-      status: 500,
-      message: err.message,
-    });
-  }
-}
-
-async function createUser(req, res) {
-  try {
-    console.log("request body", req.body);
-    const userDetails = req.body;
-    const isEmpty = Object.keys(userDetails).length === 0;
-    if (isEmpty) {
-      res.status(400).json({
-        status: 400,
-        message: "Body cannot be empty",
-      });
-    } else {
-      console.log("new user", userDetails);
-      const user = await User.create(userDetails);
-      res.status(201).json({
-        status: 201,
-        message: "User created Successfully!",
-        data: user,
-      });
-    }
-  } catch (error) {
-    res.status(500).json({
-      status: 500,
-      message: error.message,
-    });
-  }
-}
-
-async function getUserById(req, res) {
-  try {
-    const { id } = req.params;
-    console.log("userid", id);
-    const user = await User.findById(id);
-    if (!user) {
-      throw new Error("User not found");
-    }
-    res.status(200).json({
-      status: 200,
-      message: "User found",
-      data: user,
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 500,
-      message: err.message,
-    });
-  }
-}
-
-async function updateUserById(req, res) {
-  try {
-    console.log("request params", req.params);
-    const { id } = req.params;
-    const updatedUserData = req.body;
-    console.log("userid", id);
-    const updatedUser = await User.findByIdAndUpdate(
-      id,
-      updatedUserData,
-      {
-        new: true,
-      }
-    );
-    console.log("updated product", updatedUser);
-    if (!updatedUser) {
-      throw new Error("User not found");
-    } else {
-      res.status(200).json({
-        status: 200,
-        message: "User updated successfully",
-        data: updatedUser,
-      });
-    }
-  } catch (err) {
-    res.status(500).json({
-      status: 500,
-      message: err.message,
-    });
-  }
-}
-
-async function deleteUserById(req, res) {
-  try {
-    const { id } = req.params;
-    const deletedUser = await User.findByIdAndDelete(id);
-    if (!deletedUser) {
-      throw new Error("Product not found");
-    } else {
-      res.status(200).json({
-        status: 200,
-        message: "Product deleted successfully",
-        data: deletedUser,
-      });
-    }
-  } catch (err) {
-    res.status(500).json({
-      status: 500,
-      message: err.message,
-    });
-  }
-}
+const getUser = getFactory(User);
+const createUser = createFactory(User);
+const getUserById = getFactoryById(User);
+const updateUserById = updateFactoryById(User);
+const deleteUserById = deleteFactoryById(User); 
 
 module.exports = {
   getUser,
